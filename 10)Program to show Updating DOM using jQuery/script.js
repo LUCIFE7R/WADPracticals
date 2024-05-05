db0bf67dff1cfaf2a0cb64e6f0621ba20d8a64da
LUCIFE7R <ugalesanket511@gmail.com>
2024-05-05 13:03:48 +0530

    cloud

---
+++ 10)Program to show Updating DOM using jQuery/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Function to update the text when the button is clicked
+    $("#updateButton").click(function() {
+        // Update the text of the paragraph with id "demo"
+        $("#demo").text("Updated text!");
+    });
+});
